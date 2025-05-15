1d550acf9e9265b61980ef560c4504ddcffc91eb
Danyelle Angelo <danyangelofsa@gmail.com>
2025-05-15 20:51:27 -0300

    adiciona gcx a lista de ferramentas necessárias

--- prepare_external_tools.sh
+++ prepare_external_tools.sh
@@ -3,8 +3,9 @@ source utils.sh
 OS=$(uname)
 REPO_DIR="$(cd "$(dirname "$0")"; pwd)"
 echo "\n\t${GREEN}$REPO_DIR\n\t${GREEN}"
+SOURCE_DIR=$(pwd)
 
-submodules=("external/7zip" "external/GLZA") 
+submodules=("external/malloc_count" "external/GCIS" "external/ShapeSlp"  "external/sdsl-lite" "external/7zip" "external/GLZA") 
 
 need_init=0
 
@@ -32,65 +33,61 @@ fi
 #     cd external/sdsl-lite
 #     chmod +x install.sh
 #     ./install.sh
+
+#     cd $SOURCE_DIR
 # else
 #     echo -e "${BLUE}## SDSL já configurada. ${RESET}"
 # fi
 
+if [ ! -d "../GCX/" ]; then
+    echo -e "\n\t${GREEN}####### Clonando GCX ${RESET}"
+    git clone "git@github.com:DanyelleAngelo/GCX.git" ../
+else
+    echo -e "${BLUE}## GCX já foi adicionado. ${RESET}"
+fi
 
-# if [ ! -d "external/ShapeSl/build/SlpEncBuild" ]; then
-#     echo -e "\n\t${GREEN}####### ShapeSlp: Build não encontrado, configurando a lib${RESET}"
-#     cp -rf external-overrides/shapedslsp/* external/ShapeSlp/
-#     cd external/ShapeSlp
-#     git submodule update --init --recursive
-#     if [ "$(uname -s)" = "Darwin" ]; then
-#         # o código atual dessa biblioteca quebra no M1 porque __haswell__ não é definida pelo complicador
-#         echo -e "${RED}Fazendo ajustes necessários para o funcionamento da lib no M1 ${RESET}"
-#         cp ../../external-overrides/sux/common.hpp external/sux/sux/support/common.hpp
-#         cp ../../external-overrides/sux/RecSplit.hpp external/sux/sux/function/RecSplit.hpp
-#     fi
-#     mkdir -p build
-#     cd build
-#     cmake -DSDSL_INCLUDE_DIR="$REPO_DIR/external/sdsl-lite/include"  -DSDSL_LIB="$REPO_DIR/external/sdsl-lite/build/lib/libsdsl.a" ..
-#     make
-# else
-#     echo -e "${BLUE}## ShapeSlp já configurada. ${RESET}"
-# fi
 
-# if [ ! -d "external/GCIS/build/src/gcis" ]; then
-#     echo -e "\n\t${GREEN}#######  Configurando o GCIS...... ${RESET}"
-#     echo -e "\n\t####### Copiando arquivos necessários para os experimentos...."
-#     cp -rf external-overrides/gcis/* external/GCIS/
-#     echo -e "\n\t####### Instalando o GCIS....."
-#     cd external/GCIS
-#     if [ "$OS" = "Darwin" ]; then
-#         echo "Compilando para Mac (arm64)"
-#         chmod +x build-mac.sh
-#         ./build-mac.sh
-#     else
-#         ./build.sh
-#     fi
-# else
-#     echo -e "${BLUE}## GCIS já configurado. ${RESET}"
-# fi
+if [ ! -d "external/ShapeSl/build/SlpEncBuild" ]; then
+    echo -e "\n\t${GREEN}####### ShapeSlp: Build não encontrado, configurando a lib${RESET}"
 
-# if [ ! -d "external/GCIS/external/repair/build/src/repair" ]; then
-#     echo -e "\n\t${GREEN}####### Compilando Re-Pair ${RESET}"
-#     cp -rf external-overrides/repair_files/* external/GCIS/external/repair/src
-#     cd external/GCIS/external/repair
-#     rm -rf build
-#     mkdir build
-#     cd build
-#     if [ "$OS" = "Darwin" ]; then
-#         echo "Compilando para Mac (arm64)"
-#         cmake -DCMAKE_OSX_ARCHITECTURES=arm64 ..
-#     else
-#         echo "Compilando para Linux"
-#         cmake ..
-#     fi
-#     make
-# else
-#     echo -e "${BLUE}## RePair já configurada. ${RESET}"
-# fi
+    cp -rf external-overrides/shapedslsp/* external/ShapeSlp/
+    cd external/ShapeSlp
+    git submodule update --init --recursive
+    if [ "$(uname -s)" = "Darwin" ]; then
+        # o código atual dessa biblioteca quebra no M1 porque __haswell__ não é definida pelo complicador
+        echo -e "${RED}Fazendo ajustes necessários para o funcionamento da lib no M1 ${RESET}"
+        cp ../../external-overrides/sux/common.hpp external/sux/sux/support/common.hpp
+        cp ../../external-overrides/sux/RecSplit.hpp external/sux/sux/function/RecSplit.hpp
+    fi
+    mkdir -p build
+    cd build
+    cmake -DSDSL_INCLUDE_DIR="$REPO_DIR/external/sdsl-lite/include"  -DSDSL_LIB="$REPO_DIR/external/sdsl-lite/build/lib/libsdsl.a" ..
+    make
+
+    cd $SOURCE_DIR
+else
+    echo -e "${BLUE}## ShapeSlp já configurada. ${RESET}"
+fi
+
+if [ ! -d "external/GCIS/build/src/gcis" ]; then
+    echo -e "\n\t${GREEN}#######  Configurando o GCIS...... ${RESET}"
+
+    echo -e "\n\t####### Copiando arquivos necessários para os experimentos...."
+    cp -rf external-overrides/gcis/* external/GCIS/
+
+    echo -e "\n\t####### Instalando o GCIS....."
+    cd external/GCIS
+    if [ "$OS" = "Darwin" ]; then
+        echo "Compilando para Mac (arm64)"
+        chmod +x build-mac.sh
+        ./build-mac.sh
+    else
+        ./build.sh
+    fi
+    cd $SOURCE_DIR
+else
+    echo -e "${BLUE}## GCIS já configurado. ${RESET}"
+fi
 
 
 if [ ! -f "external/7zip/CPP/7zip/Bundles/Alone2/_o/7zz" ]; then
@@ -98,6 +95,7 @@ if [ ! -f "external/7zip/CPP/7zip/Bundles/Alone2/_o/7zz" ]; then
     cp -rf external-overrides/7zip/* external/7zip/
     cd external/7zip/CPP/7zip/Bundles/Alone2
     make -f makefile.gcc
+    cd $SOURCE_DIR
 else
     echo -e "${BLUE}## 7zip já configurada. ${RESET}"
 fi
@@ -110,6 +108,7 @@ if [ ! -f "external/GLZA/glza_compress" ] && [ ! -f "external/GLZA/glza_decompre
     gcc -o GLZAcompress GLZAcompress.c  -lpthread -lm
     gcc -o GLZAencode GLZAencode.c  -lpthread
     gcc -o GLZAdecode GLZAdecode.c -lpthread
+    cd $SOURCE_DIR
 
 else
     echo -e "${BLUE}## GLZA já configurada. ${RESET}"
