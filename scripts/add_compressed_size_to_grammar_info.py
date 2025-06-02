32d2455abed850003290af1a108c4bb181185bff
Danyelle Angelocd <danyangelofsa@gmail.com>
2025-06-02 20:34:21 -0300

    add compressed size and plain size columns

--- scripts/add_compressed_size_to_grammar_info.py
+++ scripts/add_compressed_size_to_grammar_info.py
@@ -1,19 +1,19 @@
 import os
 import pandas as pd
 
-caminho = '../report/2025-04-15-mac/'
+caminho = 'report/2025-06-01/'
 
-arquivos_grammar = [f for f in os.listdir(caminho) if f.endswith('gcx-grammar.csv')]
-arquivos_encoding = [f for f in os.listdir(caminho) if f.endswith('gcx-encoding.csv')]
+arquivos_grammar = [f for f in os.listdir(caminho) if f.endswith('-gcx-grammar.csv')]
+arquivos_encoding = [f for f in os.listdir(caminho) if f.endswith('-gcx-encoding.csv')]
 
 for grammar_file in arquivos_grammar:
-    encoding_file = grammar_file.replace('-grammar.csv', '-encoding.csv')
+    encoding_file = grammar_file.replace('-gcx-grammar.csv', '-gcx-encoding.csv')
     
     if encoding_file in arquivos_encoding:
         arquivo_grammar = pd.read_csv(os.path.join(caminho, grammar_file), delimiter='|')
         arquivo_encoding = pd.read_csv(os.path.join(caminho, encoding_file), delimiter='|')
-
-        arquivo_encoding_reduced = arquivo_encoding[['algorithm', 'compressed_size']]
+        print(f'{encoding_file} → colunas: {arquivo_encoding.columns.tolist()}')
+        arquivo_encoding_reduced = arquivo_encoding[['algorithm', 'compressed_size', 'plain_size']]
         merged_df = pd.merge(arquivo_grammar, arquivo_encoding_reduced, on='algorithm', how='left')
         
         merged_df.to_csv(os.path.join(caminho, grammar_file), index=False, sep='|') 
