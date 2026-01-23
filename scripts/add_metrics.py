6cc4c8fdde0af4a40bcd71472b2ac3ee18ff9a12
Danyelle Angelocd <danyangelofsa@gmail.com>
2026-01-22 21:11:08 -0300

    simplifica a lógica de adição de novas metricas

---
+++ scripts/add_metrics.py
@@ -0,0 +1,32 @@
+""""
+    Utilize esse algoritmos para adicionar ao seu dataset, métricas como:
+        - compression ratio
+        - peak memory (bytes -> MiB): espera-se que a métrica peak_comp e peak_decomp esteja em bytes
+
+"""
+
+import glob
+import pandas as pd
+import graphs.utils as ut
+
+BYTES_IN_MIB = 2 ** 20
+
+pattern = 'report/2026-01-08/*encoding.csv'
+files = glob.glob(pattern)
+
+
+for file_path in files:
+    df = pd.read_csv(file_path, sep='|')
+
+    plain_size = df['plain_size'].iloc[0]
+
+    df['compression_ratio'] = df['compressed_size'].apply(
+        lambda x: ut.compute_ratio_percentage(x, plain_size)
+    )
+    df['peak_comp_mib'] = df['peak_comp'] / BYTES_IN_MIB
+    df['peak_decomp_mib'] = df['peak_decomp'] / BYTES_IN_MIB
+    df['compressed_size_mib'] = df['compressed_size'] / BYTES_IN_MIB
+
+    df.to_csv(file_path, sep='|', index=False, float_format='%.6f')
+
+print('Conversão concluída e formatada.')
