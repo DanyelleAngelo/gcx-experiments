36b0db981202648956578cd101f09a1b0d76dfb1
Danyelle Angelocd <danyangelofsa@gmail.com>
2026-01-24 10:55:41 -0300

    permite conversão de mib a partir de valores em mb

--- scripts/add_metrics.py
+++ scripts/add_metrics.py
@@ -15,18 +15,22 @@ BYTES_IN_MIB = 2 ** 20
 pattern = 'report/2025-08-12/*encoding.csv'
 files = glob.glob(pattern)
 
+def convert_to_mib(row, column_name):
+    if row['algorithm'].strip() == 'CBT': ## os resultados antigos do CBT estão em MB
+        return row[column_name] * (1000**2 / 1024**2)
+    return row[column_name] / BYTES_IN_MIB # novos resultados, e outros algoritmos estão todos em bytes
 
 for file_path in files:
     df = pd.read_csv(file_path, sep='|')
 
     plain_size = df['plain_size'].iloc[0]
 
+    for col in ['peak_comp', 'peak_decomp', 'compressed_size']:
+        df[f'{col}_mib'] = df.apply(lambda row: convert_to_mib(row, col), axis=1)
+
     df['compression_ratio'] = df['compressed_size'].apply(
         lambda x: ut.compute_ratio_percentage(x, plain_size)
     )
-    df['peak_comp_mib'] = df['peak_comp'] / BYTES_IN_MIB
-    df['peak_decomp_mib'] = df['peak_decomp'] / BYTES_IN_MIB
-    df['compressed_size_mib'] = df['compressed_size'] / BYTES_IN_MIB
     df['plain_size_mib'] = df['plain_size'] / BYTES_IN_MIB
 
     df.to_csv(file_path, sep='|', index=False, float_format='%.6f')
